c2a93423d6b2b472bb644008260a9f4325d87f7c
Pierre David <pierreaymeric.david@gmail.com>
2020-11-12 16:16:52 +0100

    model controller

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,50 @@
+class CocktailsController < ApplicationController
+  # template for class cocktailsController < ApplicationController
+  # replace Cocktail / cocktails / cocktail
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    # @doses = @cocktail.doses
+    # @ingredients = @cocktail.ingredients
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail) # vers la show
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail) # vers la show
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path # vers l'index
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+hash_drinks = JSON.parse(open(url).read)
+ingredients = hash_drinks["drinks"]
+# puts ingredients.sample["strIngredient1"]
+50.times do
+  Ingredient.create(
+    name: ingredients.sample["strIngredient1"]
+    )
+end
+
+# Cocktail.create(name: "Cuba Libre")
+# Cocktail.create(name: "Moonlighty")
+
+# Dose.create(
+#   description: "4cl de Rhum",
+#   cocktail_id: 1,
+#   ingredient_id: 9,
+#   )
+
+# Dose.create(
+#   description: "6cl de 7-Up",
+#   cocktail_id: 1,
+#   ingredient_id: 2,
+#   )
